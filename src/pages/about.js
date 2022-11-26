68c8ea2a755e9d7fc6ecbc276e21f3b5aa58b164
Ruben Castro-Espinoza <rubencastroe@gmail.com>
2022-11-26 14:12:46 -0800

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Navbar from '../components/Navbar'
+
+const about = () => {
+    return (
+        <>
+            <Layout>
+
+                <h1>about</h1>
+            </Layout>
+        </>
+
+    )
+}
+
+export default about
---
+++ src/pages/contact.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const contact = () => {
+    return (
+        <Layout>
+            <main className='page'>
+                <section className='contact-page'>
+                    <article className='contact-info'>
+                        <h3>Want To Get In Touch?</h3>
+                        <p>
+                            I'm baby kitsch iceland umami swag pug ascot viral bruh distillery tbh. Occupy DIY waistcoat fixie poutine. Hexagon hammock try-hard unicorn waistcoat helvetica ascot meditation master cleanse salvia pickled tbh mlkshk next level cray. Post-ironic crucifix meh, beard church-key big mood master cleanse raclette dreamcatcher live-edge locavore tilde. Tilde ugh poke retro godard blue bottle 8-bit intelligentsia kickstarter unicorn.
+                        </p>
+                    </article>
+                    <article >
+                        <form className='form contact-form'>
+                            <div className='form-row'>
+                                <label htmlFor="name">Your Name</label>
+                                <input name="name" id="name" type="text"></input>
+                            </div>
+                            <div className='form-row'>
+                                <label htmlFor="email">Your Email</label>
+                                <input name="email" id="email" type="text"></input>
+                            </div>
+                            <div className='form-row'>
+                                <label htmlFor="message">Your Message</label>
+                                <textarea name="message" id="message"></textarea>
+                            </div>
+                            <button type="submit" className='btn block'>Submit</button>
+                        </form>
+                    </article>
+
+                </section>
+            </main>
+        </Layout>
+
+    )
+}
+
+export default contact
