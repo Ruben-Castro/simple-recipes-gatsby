cbecf94fcdfd70cd42f6307a84ae401577d10483
Ruben Castro-Espinoza <rubencastroe@gmail.com>
2022-11-30 22:43:25 -0800

    tags page created

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { graphql, Link } from 'gatsby'
+import setupTags from '../utils/setupTags'
+const tags = ({ data }) => {
 
-const tags = () => {
+    const tags = setupTags(data.allContentfulRecipe.nodes)
+    console.log(tags)
     return (
         <Layout>
-            <h1>Tags Page</h1>
+            <main className='page'>
+                <section className='tags-page'>
+                    {tags.map((tag, index) => {
+                        const [text, value] = tag
+                        return <Link to={`/${text}`} key={index} className="tag">
+                            <h5>{text}</h5>
+                            <p>{value} recipe</p>
+                        </Link>
+                    })}
+                </section>
+            </main>
         </Layout>
 
     )
 }
+
+
+export const query = graphql`
+  {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
+
 
 export default tags
